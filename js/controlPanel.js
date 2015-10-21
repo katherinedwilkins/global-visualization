2ef752ff73a87b1cbfe8321d78472d7a1d0c1ba5
Reginald Johnson <reginald.johnson@gmail.com>
2015-10-21 08:48:54 -0400

    g

--- js/controlPanel.js
+++ js/controlPanel.js
@@ -4,11 +4,27 @@ var client = new WindowsAzure.MobileServiceClient(
     "nEqHeNeqxcozORPJNVnEGUUWMRrHgW13"
 );
 
-function uploadFile(){
+var countryData = {};
+
+
+
+//fill in country name and code informaiton on the countryData Object
+function addCountries(data){
+	console.table(data);
+	
+	//load next set of data
+	//loadCorruptionPerceptionIndex();
+	
+}
+
+function loadCorruptionPerceptionIndex(){
 	d3.csv("./dataSets/corruption perception index.csv", function(csv){
 		console.table(csv);
 	});
+}
 
+function uploadFile(){
+	d3.csv("./dataSets/country and two digit codes.csv", addCountries);
 }
 
 document.getElementById('uploadFile').addEventListener('click', uploadFile, false);
